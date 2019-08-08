db368ccee4403f87d4eec6d28ecc4b97bb35f8c5
Sam Aryasa <sambya@aryasa.net>
2019-08-08 13:07:25 +0800

    Test scroll start-position

--- examples/demo-react-native/app.js
+++ examples/demo-react-native/app.js
@@ -10,45 +10,48 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  ScrollView,
+  Button,
 } from 'react-native';
 
 class example extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      greeting: undefined
-    };
+    this.state = {};
   }
+  
   render() {
     if (this.state.greeting) return this.renderAfterButton();
     return (
-      <View testID='welcome' style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 25, marginBottom: 30}}>
-          Welcome
-        </Text>
-        <TouchableOpacity testID='hello_button' onPress={this.onButtonPress.bind(this, 'Hello')}>
-          <Text style={{color: 'blue', marginBottom: 20}}>Say Hello</Text>
-        </TouchableOpacity>
-        <TouchableOpacity testID='world_button' onPress={this.onButtonPress.bind(this, 'World')}>
-          <Text style={{color: 'blue', marginBottom: 20}}>Say World</Text>
-        </TouchableOpacity>
+      <View style={{ flex: 1 }}>
+        <ScrollView
+          testID='SCROLL_VIEW'>
+          <View style={{ height: 200, width: 200, backgroundColor: 'blue', alignSelf: 'center', margin: 15 }} />
+          <Text style={{ margin: 15 }}>Anim officia voluptate elit minim voluptate adipisicing duis eu laboris quis voluptate incididunt veniam ad eiusmod aliquip. Eu ad ullamco eu nostrud magna tempor velit occaecat ut in aliqua laborum. Excepteur velit velit in dolore et cupidatat irure do. Eiusmod eu aute officia fugiat sint amet ex ullamco consectetur ea est tempor labore excepteur nulla cupidatat amet.</Text>
+          <View style={{ height: 200, width: 200, backgroundColor: 'blue', alignSelf: 'center', margin: 15 }} />
+          <Text style={{ margin: 15 }}>Anim officia voluptate elit minim voluptate adipisicing duis eu laboris quis voluptate incididunt veniam ad eiusmod aliquip. Eu ad ullamco eu nostrud magna tempor velit occaecat ut in aliqua laborum. Excepteur velit velit in dolore et cupidatat irure do. Eiusmod eu aute officia fugiat sint amet ex ullamco consectetur ea est tempor labore excepteur nulla cupidatat amet.</Text>
+          <View style={{ height: 200, width: 200, backgroundColor: 'blue', alignSelf: 'center', margin: 15 }} />
+          <Text style={{ margin: 15 }}>Anim officia voluptate elit minim voluptate adipisicing duis eu laboris quis voluptate incididunt veniam ad eiusmod aliquip. Eu ad ullamco eu nostrud magna tempor velit occaecat ut in aliqua laborum. Excepteur velit velit in dolore et cupidatat irure do. Eiusmod eu aute officia fugiat sint amet ex ullamco consectetur ea est tempor labore excepteur nulla cupidatat amet.</Text>
+          <View style={{ height: 200, width: 200, backgroundColor: 'blue', alignSelf: 'center', margin: 15 }} />
+          <Text style={{ margin: 15 }}>Anim officia voluptate elit minim voluptate adipisicing duis eu laboris quis voluptate incididunt veniam ad eiusmod aliquip. Eu ad ullamco eu nostrud magna tempor velit occaecat ut in aliqua laborum. Excepteur velit velit in dolore et cupidatat irure do. Eiusmod eu aute officia fugiat sint amet ex ullamco consectetur ea est tempor labore excepteur nulla cupidatat amet.</Text>
+          
+          <View 
+            testID='RED_BOX'
+            style={{ height: 200, width: 200, backgroundColor: 'red', alignSelf: 'center', margin: 15 }} />
+          <Text style={{ margin: 15 }}>Anim officia voluptate elit minim voluptate adipisicing duis eu laboris quis voluptate incididunt veniam ad eiusmod aliquip. Eu ad ullamco eu nostrud magna tempor velit occaecat ut in aliqua laborum. Excepteur velit velit in dolore et cupidatat irure do. Eiusmod eu aute officia fugiat sint amet ex ullamco consectetur ea est tempor labore excepteur nulla cupidatat amet.</Text>
+        </ScrollView>
+        
+        <View style={{ position: 'absolute', bottom: 15, right: 15, left: 15 }}>
+          <TouchableOpacity
+            style={{ backgroundColor: 'green', alignItems: 'center' }}>
+            <Text style={{ margin: 15, color: 'white' }}>
+              Submit
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
-  }
-  renderAfterButton() {
-    return (
-      <View style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 25}}>
-          {this.state.greeting}!!!
-        </Text>
-      </View>
-    );
-  }
-  onButtonPress(greeting) {
-    this.setState({
-      greeting: greeting
-    });
   }
 }
 
--- examples/demo-react-native/e2e/example.spec.js
+++ examples/demo-react-native/e2e/example.spec.js
@@ -3,17 +3,10 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
   
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-  
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-  
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it('should scroll to RED_BOX', async () => {
+    await waitFor(element(by.id('RED_BOX')))
+      .toBeVisible()
+      // .whileElement(by.id('SCROLL_VIEW')).scroll(250, 'down');
+      .whileElement(by.id('SCROLL_VIEW')).scroll(250, 'down', 0.5, 0.5);
   });
 });
